21ec76056c177a159f45447347e2fb2f9d8a2860
inakiUnzaga <inakiunzaga88@gmail.com>
2023-02-13 12:24:36 -0300

    useMemo, useCallback

---
+++ src/06-memos/CallbackHook.jsx
@@ -0,0 +1,20 @@
+import React, { useCallback, useState } from "react";
+import { ShowIncremet } from "./ShowIncremet";
+
+export const CallbackHook = () => {
+  const [counter, setCounter] = useState(10);
+
+  const increment = useCallback((value) => {
+    setCounter((c) => c + value);
+  }, []);
+
+  return (
+    <>
+      <h1> useCallback hook : {counter}</h1>
+      <hr />
+
+      <ShowIncremet increment={increment} />
+    </>
+  );
+};
+  
---
+++ src/06-memos/ShowIncremet.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const ShowIncremet =  ({ increment }) => {
+  return (
+    <button
+      className="btn btn-primary"
+      onClick={() => {
+        increment(5);
+      }}
+    >
+      Incrementar
+    </button>
+  );
+};
